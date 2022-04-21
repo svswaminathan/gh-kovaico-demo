af35c452fa6a282a3312639d19db54b7b6cc8089
Swaminathan Vetri <Swaminathan.Vetri@maersk.com>
2022-04-22 00:03:13 +0530

    Added tf scripts to create app service plan

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -17,9 +17,30 @@ provider "azurerm" {
 
 data "azurerm_client_config" "current" {}
 
-#Create Resource Group
+#Resource Group
 resource "azurerm_resource_group" "rgkovaicotrial" {
   name     = "rgkovaicotrial"
   location = "southindia"
+}
+
+# App service plan
+resource "azurerm_service_plan" "aspkovaicotrial" {
+  name                = "aspkovaicotrial"
+  resource_group_name = azurerm_resource_group.rgkovaicotrial.name
+  location            = "South India"
+  os_type             = "Linux"
+  sku_name            = "P1v2"
+}
+
+# Linux web app 
+resource "azurerm_linux_web_app" "webkovaicotrial" {
+  name                = "webkovaicotrial"
+  resource_group_name = azurerm_resource_group.aspkovaicotrial.name
+  location            = azurerm_service_plan.aspkovaicotrial.location
+  service_plan_id     = azurerm_service_plan.aspkovaicotrial.id
+
+  application_stack{
+    dotnet_version  = "6.0"
+  }
 }
   
