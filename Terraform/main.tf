76285bc524a68fecd17e6a8182d5204ee8422f66
Swaminathan Vetri <Swaminathan.Vetri@maersk.com>
2022-04-21 22:32:45 +0530

    Added simple tf file

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,1 +1,25 @@
 # terraform scripts to follow here
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "rg-kovaico-tfstate"
+    storage_account_name = "stkovaicotfstate"
+    container_name       = "stckovaicotfstate"
+    key                  = "stckovaicotfstate.tfstate"
+  }
+}
+
+provider "azurerm" {
+  # The "feature" block is required for AzureRM provider 2.x.
+  # If you're using version 1.x, the "features" block is not allowed.
+  version = "~>2.0"
+  features {}
+}
+
+data "azurerm_client_config" "current" {}
+
+#Create Resource Group
+resource "azurerm_resource_group" "rgkovaicotrial" {
+  name     = "rgkovaicotrial"
+  location = "southindia"
+}
+  
